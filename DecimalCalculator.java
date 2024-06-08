ed6a4f0f949dc275cf83345b52c4265ffe76e066
balazspopovics00 <105217880+balazspopovics00@users.noreply.github.com>
2024-06-08 17:37:34 +0200

    upload project

--- DecimalCalculator.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class DecimalCalculator {
-
-    public int decimalAdd(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber, int fifthNumber) {
-        return firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber;
-    }
-}
